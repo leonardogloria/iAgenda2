fe5dc676f8e222b7ff33d5135e51ad038c021f80
leonardo.gloria@globo.com <leonardo.gloria@globo.com>
2015-01-15 00:16:14 -0200

    Atributos da classe Estabelecimento

--- grails-app/domain/br/com/iagenda/Estabelecimento.groovy
+++ grails-app/domain/br/com/iagenda/Estabelecimento.groovy
@@ -2,8 +2,19 @@ package br.com.iagenda
 
 class Estabelecimento {
 
-    String nome
+    String razaoSocial
     String cnpj
+    String email
+    String homePage
+    String pessoaContato
+
+    //TODO Criar classe de Domonio de Telefone
+    String telefone
+    String telefoneContato
+
+    //Todo Criar Classe de dominio de endereco
+    String endereco
+
     TipoEstabelecimento tipoEstabelecimento
 
     static hasMany = [servicos:Servico]
