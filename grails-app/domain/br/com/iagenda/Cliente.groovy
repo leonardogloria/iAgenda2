8fe6ad425e6e57e283ff4cf4a949c5f78eabaae7
leonardo.gloria@globo.com <leonardo.gloria@globo.com>
2015-01-15 00:32:59 -0200

    Atributos da classe Cliente

--- grails-app/domain/br/com/iagenda/Cliente.groovy
+++ grails-app/domain/br/com/iagenda/Cliente.groovy
@@ -1,7 +1,13 @@
 package br.com.iagenda
 
 class Cliente {
+
     String nome
+    String sobrenome
+    String sexo //Todo criar enum para sexo
+    String cpf //Todo criar constraint para cpf
+    String telefone //Todo criar classe para telefone
+    String email
 
     static constraints = {
     }
